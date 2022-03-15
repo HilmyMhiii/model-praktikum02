86bf97cedfc2280d43811c8020366461347a63c5
HilmyMhiii <hilmymhi874@gmail.com>
2022-03-15 22:56:07 +0700

    migration

---
+++ app/Http/Controllers/Praktikum2Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\praktikum2;
+use Illuminate\Http\Request;
+
+class Praktikum2Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\praktikum2  $praktikum2
+     * @return \Illuminate\Http\Response
+     */
+    public function show(praktikum2 $praktikum2)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\praktikum2  $praktikum2
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(praktikum2 $praktikum2)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\praktikum2  $praktikum2
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, praktikum2 $praktikum2)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\praktikum2  $praktikum2
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(praktikum2 $praktikum2)
+    {
+        //
+    }
+}
---
+++ database/seeders/praktikum2Seeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\praktikum2;
+use Illuminate\Database\Seeder;
+
+class praktikum2Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        praktikum2::create([
+            'judul' => 'Buah - Buahan',
+            'deskripsi' => 'Berbagai macam buah terdapat di web ini',
+        ]);
+    }
+}
