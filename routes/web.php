b1b183fad1dc1480360c5f43957a3c1f428c83fb
HilmyMhiii <hilmymhi874@gmail.com>
2022-03-15 22:19:34 +0700

    view page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route view
+Route::view('/about','about');
+Route::view('/cart','cart');
+Route::view('/checkout','checkout');
+Route::view('/contact-us','contact-us');
+Route::view('/gallery','gallery');
+Route::view('/my-account','my-account');
+Route::view('/shop-detail','shop-detail');
+Route::view('/shop','shop');
+Route::view('/wishlist','wishlist');
+
 
 Auth::routes();
 
